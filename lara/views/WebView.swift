7f06af188f6a951c6f64653078a28c36474763ec
jurre111 <jurredijkstra@mijnheem.nl>
2026-05-18 21:20:53 +0200

    fixed?

--- lara/views/WebView.swift
+++ lara/views/WebView.swift
@@ -5,11 +5,12 @@ struct WebView: View {
     @State private var serverstarted = false
     @State private var path: String = "/"
     @State private var webdav: GCDWebDAVServer?
+    @State private var url: URL?
     var body: some View {
         List {
             Section {
                 TextField("Path", text: $path)
-                Button(serverstarted ? "Running..." : "Start WebDAV Server") {
+                Button(serverstarted && url ? "Running on \(url!.absoluteString)" : "Start WebDAV Server") {
                     initWebServer(path)
                 }
                 .disabled(serverstarted)
@@ -22,7 +23,8 @@ struct WebView: View {
         if server.start() {
             webdav = server
             serverstarted = true
-            Alertinator.shared.alert(title: "Success!", body: "Enter \(webdav?.serverURL.absoluteString ?? "") in your webdav client")
+            url = webdav?.serverURL
+            Alertinator.shared.alert(title: "Success!", body: "Enter \(url?.absoluteString ?? "") in your webdav client")
         } else {
             Alertinator.shared.alert(title: "Error", body: "Failed to start server. Make sure the path is correct and you have the necessary permissions.")
         }
