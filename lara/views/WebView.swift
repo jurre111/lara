172ed0c179b6997c7d492a53a6f9e29c137bda02
jurre111 <jurredijkstra@mijnheem.nl>
2026-05-18 21:14:40 +0200

    ye

--- lara/views/WebView.swift
+++ lara/views/WebView.swift
@@ -22,7 +22,7 @@ struct WebView: View {
         if server.start() {
             webdav = server
             serverstarted = true
-            Alertinator.shared.alert(title: "Success!", body: "Enter \(webdav?.serverURL ?? "") in your webdav client")
+            Alertinator.shared.alert(title: "Success!", body: "Enter \(webdav?.serverURL.absoluteString ?? "") in your webdav client")
         } else {
             Alertinator.shared.alert(title: "Error", body: "Failed to start server. Make sure the path is correct and you have the necessary permissions.")
         }
