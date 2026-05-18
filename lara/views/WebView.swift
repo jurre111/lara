98184a839d15cb6e0ef0b695a945366b8b25c098
jurre111 <jurredijkstra@mijnheem.nl>
2026-05-18 21:06:12 +0200

    webdav

--- lara/views/WebView.swift
+++ lara/views/WebView.swift
@@ -3,28 +3,28 @@ import Foundation
 
 struct WebView: View {
     @State private var serverstarted = false
+    @State private var path: String = "/"
+    @State private var webdav: GCDWebDAVServer?
     var body: some View {
         List {
             Section {
-                Button(serverstarted ? "Running..." : "Start Web Server") {
-                    initWebServer()
+                TextField("Path", text: $path)
+                Button(serverstarted ? "Running..." : "Start WebDAV Server") {
+                    initWebServer(path)
                 }
                 .disabled(serverstarted)
             }
         }
     }
 
-    func initWebServer() {
-
-        let webServer = GCDWebServer()
-
-        webServer.addDefaultHandler(forMethod: "GET", request: GCDWebServerRequest.self, processBlock: {request in
-                return GCDWebServerDataResponse(html:"<html><body><p>Hello World</p></body></html>")
-                
-            })
-            
-        webServer.start(withPort: 8080, bonjourName: "GCD Web Server")
-        serverstarted = true
-        Alertinator.shared.alert(title: "Success!", body: "Visit \(webServer.serverURL) in your web browser")
+    func initWebServer(_ path: String) {
+        let server = GCDWebDAVServer(uploadDirectory: path)
+        if server.start() {
+            webdav = server
+            serverstarted = true
+            Alertinator.shared.alert(title: "Success!", body: "Enter \(webdav?.serverURL ?? "") in your webdav client")
+        } else {
+            Alertinator.shared.alert(title: "Error", body: "Failed to start server. Make sure the path is correct and you have the necessary permissions.")
+        }
     }
 }
