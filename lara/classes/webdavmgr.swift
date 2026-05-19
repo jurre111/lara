3086d72c0cc86aa2d129b942c2e064344b18b723
jurre111 <jurredijkstra@mijnheem.nl>
2026-05-19 08:43:29 +0200

    k

--- lara/classes/webdavmgr.swift
+++ lara/classes/webdavmgr.swift
@@ -22,7 +22,7 @@ final class webdavmgr: ObservableObject {
 
         var error: NSError? = nil
         let options: [String: Any] = [GCDWebServerOption_AutomaticallySuspendInBackground: false]
-        let started = server.start(options: options, error: &error)
+        let started = server.start(options: options)
         
         DispatchQueue.main.async {
             if started {
