2e9f7d5c0098b98a537b2d97f833db00aa8530a7
jurre111 <jurredijkstra@mijnheem.nl>
2026-08-24 14:03:33 +0200

    skip files?

--- lara/views/fm/DirectoryView.swift
+++ lara/views/fm/DirectoryView.swift
@@ -445,9 +445,11 @@ struct santanderdirview: View {
     }
 
     private func chmod(entry: santanderitem, mode: UInt16, recursive: Bool = true) -> Bool {
-        santanderfs.clearImmutableIfPossible(atPath: entry.path)
-        let ok = entry.path.withCString { apfs_mod($0, mode) == 0 }
-        laramgr.shared.logmsg("Chmod \(entry.path) \(ok ? "Ok" : "Failed")")
+        if !recursive || entry.isdir {
+            santanderfs.clearImmutableIfPossible(atPath: entry.path)
+            let ok = entry.path.withCString { apfs_mod($0, mode) == 0 }
+            laramgr.shared.logmsg("Chmod \(entry.path) \(ok ? "Ok" : "Failed")")
+        }
         if recursive && entry.isdir {
             let contents = santanderfs.listdir(item: entry, readsbx: readsbx)
             for item in contents.items {
